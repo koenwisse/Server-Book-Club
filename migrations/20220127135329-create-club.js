37d716b8384d296d98d153e53d14f7199e96ccd2
NaravaTejasri <tejasrisony17@gmail.com>
2022-01-27 16:17:43 +0100

    added allowNull to the existing code

--- migrations/20220127135329-create-club.js
+++ migrations/20220127135329-create-club.js
@@ -1,30 +1,32 @@
-'use strict';
+"use strict";
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('clubs', {
+    await queryInterface.createTable("clubs", {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       name: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false,
       },
       joinCode: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false,
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('clubs');
-  }
+    await queryInterface.dropTable("clubs");
+  },
 };
