27efd3edba81a2eaa48d8802b9847ea7b4e1bc5d
NaravaTejasri <tejasrisony17@gmail.com>
2022-01-28 14:00:49 +0100

    added data to the club,bookdiscussion and bookdiscussioncomments

---
+++ seeders/20220128104640-some-clubs.js
@@ -0,0 +1,44 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert(
+      "clubs",
+      [
+        {
+          name: "Teja Club",
+          joinCode: "join the club",
+          adminId: 1,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          name: "Koen Club",
+          adminId: 2,
+          joinCode: "join the club",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          name: "Susana Club",
+          adminId: 1,
+          joinCode: "join the club",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          name: "Andre Club",
+          joinCode: "join the club",
+          adminId: 2,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete("clubs", null, {});
+  },
+};
