b22cc6d691d5986c324a8359858df0b4878e93b1
NaravaTejasri <tejasrisony17@gmail.com>
2022-01-29 14:35:51 +0100

    created CRUD request for the club

--- routers/club.js
+++ routers/club.js
@@ -6,10 +6,10 @@ const UserClub = require("../models").userClub;
 
 const router = new Router();
 
-// Get all categories
+// Get all clubs
 router.get("/", async (req, res) => {
   try {
-    const clubs = await Club.findAll();
+    const clubs = await Club.findAll({ include: User });
     console.log(clubs);
     res.status(200).send({ message: "ok", clubs });
   } catch (e) {
@@ -17,7 +17,7 @@ router.get("/", async (req, res) => {
   }
 });
 
-//post request to create thye club
+//post request to create the club
 router.post("/", auth, async (req, res) => {
   try {
     //const userId = req.body.userId;
@@ -32,7 +32,7 @@ router.post("/", auth, async (req, res) => {
     const createdClub = await Club.create({
       name,
       joinCode,
-      userId,
+      userId: userId,
     });
 
     const clubId = createdClub.id;
@@ -42,13 +42,49 @@ router.post("/", auth, async (req, res) => {
       userId,
       clubId,
     });
-
-    /* const resolvedPromises = await Promise.all(userClubPromise);
-    console.log("resolved", resolvedPromises); */
-
     res.status(201).send({ message: "New Club Created", club: createdClub });
   } catch (e) {
     console.log("Error", e);
+  }
+});
+
+//user can edit the club
+router.patch("/:id", auth, async (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+  const club = await Club.findByPk(id);
+  if (!club.userId === req.user.id) {
+    return res
+      .status(403)
+      .send({ message: "You are not authorized to update this club" });
+  }
+  if (!club) {
+    return res.status(404).send("Club not found");
+  }
+  const { name, joinCode } = req.body;
+
+  await club.update({ name, joinCode });
+
+  return res.status(200).send({ club });
+});
+
+//user can delete the club
+router.delete("/:clubId", auth, async (req, res, next) => {
+  try {
+    const { clubId } = req.params;
+    console.log(clubId);
+    const club = await Club.findByPk(clubId);
+    if (!club) {
+      return res.status(404).send("Club not found");
+    }
+    // Check if this user is the owner of the club
+    if (!club.userId === req.user.id) {
+      return res.status(401).send("You're not authorized to delete this club");
+    }
+    await club.destroy();
+    res.send({ message: "ok", clubId });
+  } catch (e) {
+    next(e);
   }
 });
 
